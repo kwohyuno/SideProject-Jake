c482cf9b441a0d2d7da71dad979e07990b7f6b30
kwohyuno <121947555+kwohyuno@users.noreply.github.com>
2024-10-25 08:34:56 -0700

    Board main page (view)

--- src/main/frontend/src/pages/board/Board.js
+++ src/main/frontend/src/pages/board/Board.js
@@ -57,137 +57,187 @@ function Board(props) {
             <div className="board-header">
 
                 <div className="board-header-logo">
-                    <img className="board-header-logo-image" alt="" src={'https://projectjakeassets.s3.ap-northeast-2.amazonaws.com/src/board_assets/header-icon.svg'}/>
+                    <img className="board-header-logo-image" alt=""
+                         src={'https://projectjakeassets.s3.ap-northeast-2.amazonaws.com/src/board_assets/header-icon.svg'}/>
                 </div>
                 <div className="board-header-searchbar">
-                    <img className="board-header-searchbar-icon" alt="" src={'https://projectjakeassets.s3.ap-northeast-2.amazonaws.com/src/board_assets/searchicon.svg'}/>
+                    <img className="board-header-searchbar-icon" alt=""
+                         src={'https://projectjakeassets.s3.ap-northeast-2.amazonaws.com/src/board_assets/searchicon.svg'}/>
                     <input className="board-header-searchbar-input" type="text" placeholder="Search posts"/>
                 </div>
                 <div className="board-header-posticon">
                     <button className="board-header-posticon-btn" onClick={handleCreate}>+ Create</button>
                 </div>
                 <div className="board-header-chaticon">
-                    <img className="board-header-chaticon" alt="" src={'https://projectjakeassets.s3.ap-northeast-2.amazonaws.com/src/board_assets/chaticon.svg'} style={{ cursor: 'pointer' }} onClick={()=>navi("/message")}/>
+                    <img className="board-header-chaticon" alt=""
+                         src={'https://projectjakeassets.s3.ap-northeast-2.amazonaws.com/src/board_assets/chaticon.svg'}
+                         style={{cursor: 'pointer'}} onClick={() => navi("/message")}/>
 
                 </div>
                 <div className="board-header-mypagebtn">
                     <button className="board-header-mypagebtn-btn">My Page</button>
                 </div>
                 <div className="board-header-logoutbtn">
-                <button className="board-header-logoutbtn-btn" onClick={handleLogout}>Log out</button>
+                    <button className="board-header-logoutbtn-btn" onClick={handleLogout}>Log out</button>
                 </div>
 
             </div>
+            {/*<div className="board-body">*/}
+
+            {/*    <div className="board-body-box">*/}
+            {/*        <div className="board-body-box-post1">*/}
+
+
+            {/*            <div className="board-body-box-profile">*/}
+            {/*                <img className="board-body-box-profile-img" alt={userData.name}*/}
+            {/*                     onClick={() => handleClickOpen(userData)} style={{cursor: 'pointer'}}*/}
+            {/*                     src={'https://projectjakeassets.s3.ap-northeast-2.amazonaws.com/src/board_assets/profileimg.svg'}/>*/}
+
+
+            {/*                <div className="board-body-box-profile-id" onClick={() => handleClickOpen(userData)}*/}
+            {/*                     style={{cursor: 'pointer'}}> r/AITAH*/}
+            {/*                </div>*/}
+            {/*                <div className="board-body-box-profile-writtendate"> 12hr ago</div>*/}
+
+            {/*                <Dialog open={open} onClose={handleClose}>*/}
+            {/*                    <DialogTitle>{selectedUser?.name}</DialogTitle>*/}
+            {/*                    <DialogContent>*/}
+            {/*                        <img src={selectedUser?.photo} alt={selectedUser?.name}*/}
+            {/*                             style={{width: '100px', height: '100px'}}/>*/}
+            {/*                        <p>{selectedUser?.email}</p>*/}
+            {/*                        <p>{selectedUser?.id}</p>*/}
+            {/*                        /!* 유저의 프로필 정보 등 원하는 내용을 추가 *!/*/}
+            {/*                    </DialogContent>*/}
+            {/*                    <DialogActions>*/}
+            {/*                        <Button onClick={handleClose} color="primary">*/}
+            {/*                            닫기*/}
+            {/*                        </Button>*/}
+            {/*                    </DialogActions>*/}
+            {/*                </Dialog>*/}
+
+            {/*            </div>*/}
+
+            {/*            {boardList.slice(-1).map((board, index) => (*/}
+            {/*                <div key={index} onClick={()=>{*/}
+            {/*                    navi(`/board/detail/${board.id}`);*/}
+            {/*                }}*/}
+            {/*                style={{cursor:"pointer"}}*/}
+            {/*                >*/}
+            {/*                    <div className="board-body-box-subject">*/}
+            {/*                        {board.title}*/}
+            {/*                    </div>*/}
+            {/*                    <div className="board-body-box-contents">*/}
+            {/*                        {board.content}*/}
+            {/*                    </div>*/}
+            {/*                </div>*/}
+            {/*            ))}*/}
+
+            {/*        </div>*/}
+
+
+            {/*    </div>*/}
+
+            {/*    <div className="board-body-box2">*/}
+            {/*        <div className="board-body-box-post2" >*/}
+            {/*            <div className="board-body-box-profile">*/}
+            {/*                <img className="board-body-box-profile-img" alt={userData.name}*/}
+            {/*                     onClick={() => handleClickOpen(userData)} style={{cursor: 'pointer'}}*/}
+            {/*                     src={'https://projectjakeassets.s3.ap-northeast-2.amazonaws.com/src/board_assets/profileimg.svg'}/>*/}
+
+
+            {/*                <div className="board-body-box-profile-id" onClick={() => handleClickOpen(userData)}*/}
+            {/*                     style={{cursor: 'pointer'}}> r/AITAH*/}
+            {/*                </div>*/}
+            {/*                <div className="board-body-box-profile-writtendate"> 12hr ago</div>*/}
+
+            {/*                <Dialog open={open} onClose={handleClose}>*/}
+            {/*                    <DialogTitle>{selectedUser?.name}</DialogTitle>*/}
+            {/*                    <DialogContent>*/}
+            {/*                        <img src={selectedUser?.photo} alt={selectedUser?.name}*/}
+            {/*                             style={{width: '100px', height: '100px'}}/>*/}
+            {/*                        <p>{selectedUser?.email}</p>*/}
+            {/*                        <p>{selectedUser?.id}</p>*/}
+            {/*                        /!* 유저의 프로필 정보 등 원하는 내용을 추가 *!/*/}
+            {/*                    </DialogContent>*/}
+            {/*                    <DialogActions>*/}
+            {/*                        <Button onClick={handleClose} color="primary">*/}
+            {/*                            닫기*/}
+            {/*                        </Button>*/}
+            {/*                    </DialogActions>*/}
+            {/*                </Dialog>*/}
+
+            {/*            </div>*/}
+
+            {/*            {boardList.slice(-2,-1).map((board, index) => (*/}
+            {/*                <div key={index} onClick={()=>{*/}
+            {/*                    navi(`/board/detail/${board.id}`);*/}
+            {/*                }}*/}
+            {/*                     style={{cursor:"pointer"}}>*/}
+            {/*                    <div className="board-body-box-subject">*/}
+            {/*                        {board.title}*/}
+            {/*                    </div>*/}
+            {/*                    <div className="board-body-box-contents">*/}
+            {/*                        {board.content}*/}
+            {/*                    </div>*/}
+            {/*                </div>*/}
+            {/*            ))}*/}
+
+            {/*        </div>*/}
+            {/*    </div>*/}
+
+            {/*    <div className="board-body-pages">*/}
+            {/*        1 2 3 4 5 ... 12*/}
+            {/*    </div>*/}
+
+            {/*</div>*/}
+
+
             <div className="board-body">
-
-                <div className="board-body-box">
-                    <div className="board-body-box-post1">
-
-
-
-                        <div className="board-body-box-profile">
-                            <img className="board-body-box-profile-img" alt={userData.name}
-                                 onClick={() => handleClickOpen(userData)} style={{cursor: 'pointer'}}
-                                 src={'https://projectjakeassets.s3.ap-northeast-2.amazonaws.com/src/board_assets/profileimg.svg'}/>
-
-
-                            <div className="board-body-box-profile-id" onClick={() => handleClickOpen(userData)}
-                                 style={{cursor: 'pointer'}}> r/AITAH
+                {boardList.map((board, index) => (
+                    <div key={index} className="board-body-box" onClick={() => navi(`/board/detail/${board.id}`)}
+                         style={{cursor: 'pointer'}}>
+                        <div className="board-body-box-post">
+                            <div className="board-body-box-profile">
+                                <img
+                                    className="board-body-box-profile-img"
+                                    alt={userData.name}
+                                    onClick={() => handleClickOpen(userData)}
+                                    style={{cursor: 'pointer'}}
+                                    src={'https://projectjakeassets.s3.ap-northeast-2.amazonaws.com/src/board_assets/profileimg.svg'}
+                                />
+                                <div className="board-body-box-profile-id" onClick={() => handleClickOpen(userData)}
+                                     style={{cursor: 'pointer'}}>
+                                    {board.authorId}
+                                </div>
+                                <div className="board-body-box-profile-writtendate">{board.createdAt}</div>
                             </div>
-                            <div className="board-body-box-profile-writtendate"> 12hr ago</div>
 
-                            <Dialog open={open} onClose={handleClose}>
-                                <DialogTitle>{selectedUser?.name}</DialogTitle>
-                                <DialogContent>
-                                    <img src={selectedUser?.photo} alt={selectedUser?.name}
-                                         style={{width: '100px', height: '100px'}}/>
-                                    <p>{selectedUser?.email}</p>
-                                    <p>{selectedUser?.id}</p>
-                                    {/* 유저의 프로필 정보 등 원하는 내용을 추가 */}
-                                </DialogContent>
-                                <DialogActions>
-                                    <Button onClick={handleClose} color="primary">
-                                        닫기
-                                    </Button>
-                                </DialogActions>
-                            </Dialog>
-
+                            <div className="board-body-box-subject">
+                                {board.title}
+                            </div>
+                            <div className="board-body-box-contents">
+                                {board.content}
+                            </div>
                         </div>
-
-                        {boardList.slice(-1).map((board, index) => (
-                            <div key={index} onClick={()=>{
-                                navi(`/board/detail/${board.id}`);
-                            }}
-                            style={{cursor:"pointer"}}
-                            >
-                                <div className="board-body-box-subject">
-                                    {board.title}
-                                </div>
-                                <div className="board-body-box-contents">
-                                    {board.content}
-                                </div>
-                            </div>
-                        ))}
-
                     </div>
+                ))}
 
-
-                </div>
-
-                <div className="board-body-box2">
-                    <div className="board-body-box-post2" >
-                        <div className="board-body-box-profile">
-                            <img className="board-body-box-profile-img" alt={userData.name}
-                                 onClick={() => handleClickOpen(userData)} style={{cursor: 'pointer'}}
-                                 src={'https://projectjakeassets.s3.ap-northeast-2.amazonaws.com/src/board_assets/profileimg.svg'}/>
-
-
-                            <div className="board-body-box-profile-id" onClick={() => handleClickOpen(userData)}
-                                 style={{cursor: 'pointer'}}> r/AITAH
-                            </div>
-                            <div className="board-body-box-profile-writtendate"> 12hr ago</div>
-
-                            <Dialog open={open} onClose={handleClose}>
-                                <DialogTitle>{selectedUser?.name}</DialogTitle>
-                                <DialogContent>
-                                    <img src={selectedUser?.photo} alt={selectedUser?.name}
-                                         style={{width: '100px', height: '100px'}}/>
-                                    <p>{selectedUser?.email}</p>
-                                    <p>{selectedUser?.id}</p>
-                                    {/* 유저의 프로필 정보 등 원하는 내용을 추가 */}
-                                </DialogContent>
-                                <DialogActions>
-                                    <Button onClick={handleClose} color="primary">
-                                        닫기
-                                    </Button>
-                                </DialogActions>
-                            </Dialog>
-
-                        </div>
-
-                        {boardList.slice(-2,-1).map((board, index) => (
-                            <div key={index} onClick={()=>{
-                                navi(`/board/detail/${board.id}`);
-                            }}
-                                 style={{cursor:"pointer"}}>
-                                <div className="board-body-box-subject">
-                                    {board.title}
-                                </div>
-                                <div className="board-body-box-contents">
-                                    {board.content}
-                                </div>
-                            </div>
-                        ))}
-
-                    </div>
-                </div>
-
-                <div className="board-body-pages">
-                    1 2 3 4 5 ... 12
-                </div>
-
+                {/* 프로필 정보 다이얼로그 */}
+                <Dialog open={open} onClose={handleClose}>
+                    <DialogTitle>{selectedUser?.name}</DialogTitle>
+                    <DialogContent>
+                        <img src={selectedUser?.photo} alt={selectedUser?.name}
+                             style={{width: '100px', height: '100px'}}/>
+                        <p>{selectedUser?.email}</p>
+                        <p>{selectedUser?.id}</p>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} color="primary">
+                            닫기
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
-
         </div>
     );
 }
